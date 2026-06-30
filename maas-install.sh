bc4fa9083a41a180eafc1c269c296d452bb891be
Alessandro Marcolini <alessandromarcolini99@gmail.com>
2026-06-30 13:13:45 +0200

    feat: dispatch maas-install.sh on snap/deb method arg

--- maas-install.sh
+++ maas-install.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 #
-# Install MAAS snap, connect interfaces, and initialize region+rack
-# Usage: /scripts/maas-install.sh <DB_IP>
-# This is mounted at /scripts inside containers via LXD profile
+# Install MAAS (snap or deb) and initialize region+rack.
+# Usage:
+#   /scripts/maas-install.sh snap <DB_IP> <MAAS_CHANNEL>
+#   /scripts/maas-install.sh deb  <PPA> <BRANCH>
+# This is mounted at /scripts inside containers via the LXD profile.
 
-DB_IP="$1"
-MAAS_CHANNEL="$2"
+METHOD="$1"
 
-HOST_IP="$(hostname -I | cut -d' ' -f1)"
+clone_maas() {
+    # $1 = git branch to clone into /work
+    sudo git clone --branch "$1" --depth 10 --shallow-submodules \
+        https://github.com/canonical/maas /work
+    sudo chown -R ubuntu:ubuntu /work
+}
 
-TRACK="${MAAS_CHANNEL%%/*}"
-if [ "$TRACK" = "latest" ]; then
-    BRANCH="master"
-else
-    BRANCH="$TRACK"
-fi
+case "$METHOD" in
+    snap)
+        DB_IP="$2"
+        MAAS_CHANNEL="$3"
+        HOST_IP="$(hostname -I | cut -d' ' -f1)"
 
-sudo git clone --branch "$BRANCH" --depth 10 --shallow-submodules https://github.com/canonical/maas /work
-sudo chown -R ubuntu:ubuntu /work
+        TRACK="${MAAS_CHANNEL%%/*}"
+        if [ "$TRACK" = "latest" ]; then
+            BRANCH="master"
+        else
+            BRANCH="$TRACK"
+        fi
 
-sudo snap install --beta snapd
-sudo snap install --beta core26
-sudo snap install maas --channel="$MAAS_CHANNEL"
-/work/utilities/connect-snap-interfaces
+        clone_maas "$BRANCH"
 
-sudo maas init region+rack \
-    --database-uri="postgres://maas:maas@${DB_IP}/maasdb" \
-    --maas-url="http://$HOST_IP:5240/MAAS"
+        sudo snap install --beta snapd
+        sudo snap install --beta core26
+        sudo snap install maas --channel="$MAAS_CHANNEL"
+        /work/utilities/connect-snap-interfaces
+
+        sudo maas init region+rack \
+            --database-uri="postgres://maas:maas@${DB_IP}/maasdb" \
+            --maas-url="http://$HOST_IP:5240/MAAS"
+        ;;
+    deb)
+        PPA="$2"
+        BRANCH="$3"
+
+        clone_maas "$BRANCH"
+
+        sudo add-apt-repository -y "$PPA"
+        sudo apt update
+        sudo apt install -y maas
+        sudo maas init --skip-admin --candid-domain '' --rbac-url ''
+        ;;
+    *)
+        echo "Usage: $0 snap <DB_IP> <MAAS_CHANNEL> | deb <PPA> <BRANCH>" >&2
+        exit 1
+        ;;
+esac
