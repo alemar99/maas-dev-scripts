d1de364e3f2bbfe3c1b545832d6b7902cc90503e
Alessandro Marcolini <alessandromarcolini99@gmail.com>
2026-08-21 15:09:51 +0200

    fix: revert postgres_setup script changes

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -7,8 +7,8 @@ set -e
 
 sudo apt update
 sudo apt install -y postgresql
-sudo -u postgres psql -c "SELECT 'CREATE USER maas WITH ENCRYPTED PASSWORD ''maas''' WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname='maas')\gexec"
-sudo -u postgres psql -c "SELECT 'CREATE DATABASE maasdb OWNER maas' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname='maasdb')\gexec"
+sudo -u postgres psql -c "create user maas with encrypted password 'maas'"
+sudo -u postgres createdb -O maas maasdb
 PGVER=$(ls -1 /etc/postgresql | tail -1)
 echo -e "\nhost maasdb maas 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/$PGVER/main/pg_hba.conf
 
