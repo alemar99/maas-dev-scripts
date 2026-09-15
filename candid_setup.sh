c2a15c54dd24036e8bc0d956409e07f930b8483a
Alessandro Marcolini <alessandromarcolini99@gmail.com>
2026-09-15 13:30:07 +0200

    fix: fix candid setup
    Also use shellcheck -x in pre-commit to make it able to follow sourced
    files

--- candid_setup.sh
+++ candid_setup.sh
@@ -7,3 +7,13 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR"/base_candid_rbac_setup.sh
+
+sudo cp /var/snap/candid/current/admin.keys /root/
+sudo candid -a /root/admin.keys create-agent -f /root/maas.agent
+USERNAME=$(sudo cat /root/maas.agent | jq '.agents[0].username')
+sudo candid -a /root/admin.keys acl grant read-user-groups "$USERNAME"
+sudo candid -a /root/admin.keys acl grant read-user "$USERNAME"
+
+sudo cp /var/snap/candid/current/admin.keys /tmp/snap-private-tmp/snap.maas/tmp/.
+sudo maas configauth --rbac-url '' --candid-agent-file /tmp/admin.keys \
+    --candid-domain '' --candid-admin-group admin
